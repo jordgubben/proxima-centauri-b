d45c68c924e4d24b3cd628d6763b6438c60984e2
Jakob Eklund <jordgubben@users.noreply.github.com>
2024-05-08 19:20:01 +0200

    Unify 'space' implementation in single c-file.
    Simplify build by putting everything but the starting points into
    a single C source file called 'implement_space.c'

---
+++ space/implement_space.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define IN_PLAY_S
+#include "space.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <raylib.h>
+#include <math.h>
+
+const float tau = 6.28;
+
+////////////////////////////////////////////////////////////////////////////////
+// Model 'space' subsystem
+
+void init_space(space_t* space) {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void process_space_input(space_t* space) {
+}
+
+
+void update_space(float dt, space_t* space) {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void  render_space(space_t const* space) {
+}
+
+
--- space/model_space.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-#define IN_PLAY_S
-#include "space.h"
-
-void init_space(space_t* space) {
-}
-
